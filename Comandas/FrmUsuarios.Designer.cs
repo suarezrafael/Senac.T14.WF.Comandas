27f33e0af81a79698ec4b4fc583fbae6cefd9811
Rafael Vieira Suarez <rafaelv_s@hotmail.com>
2024-07-11 20:06:37 -0300

    metodo atualizar usuario

--- Comandas/FrmUsuarios.Designer.cs
+++ Comandas/FrmUsuarios.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             btnVoltar = new ReaLTaiizor.Controls.CyberButton();
+            btnSalvar = new ReaLTaiizor.Controls.CyberButton();
+            btnNovo = new ReaLTaiizor.Controls.CyberButton();
             SuspendLayout();
             // 
             // btnVoltar
@@ -72,11 +74,95 @@
             btnVoltar.Timer_RGB = 300;
             btnVoltar.Click += btnVoltar_Click;
             // 
+            // btnSalvar
+            // 
+            btnSalvar.Alpha = 20;
+            btnSalvar.BackColor = Color.Transparent;
+            btnSalvar.Background = true;
+            btnSalvar.Background_WidthPen = 4F;
+            btnSalvar.BackgroundPen = true;
+            btnSalvar.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnSalvar.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnSalvar.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnSalvar.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnSalvar.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnSalvar.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnSalvar.Effect_1 = true;
+            btnSalvar.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnSalvar.Effect_1_Transparency = 25;
+            btnSalvar.Effect_2 = true;
+            btnSalvar.Effect_2_ColorBackground = Color.White;
+            btnSalvar.Effect_2_Transparency = 20;
+            btnSalvar.Font = new Font("Arial", 11F);
+            btnSalvar.ForeColor = Color.FromArgb(245, 245, 245);
+            btnSalvar.Lighting = false;
+            btnSalvar.LinearGradient_Background = false;
+            btnSalvar.LinearGradientPen = false;
+            btnSalvar.Location = new Point(206, 154);
+            btnSalvar.Name = "btnSalvar";
+            btnSalvar.PenWidth = 15;
+            btnSalvar.Rounding = true;
+            btnSalvar.RoundingInt = 70;
+            btnSalvar.Size = new Size(130, 50);
+            btnSalvar.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnSalvar.TabIndex = 1;
+            btnSalvar.Tag = "Cyber";
+            btnSalvar.TextButton = "Salvar";
+            btnSalvar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnSalvar.Timer_Effect_1 = 5;
+            btnSalvar.Timer_RGB = 300;
+            btnSalvar.Click += btnSalvar_Click;
+            // 
+            // btnNovo
+            // 
+            btnNovo.Alpha = 20;
+            btnNovo.BackColor = Color.Transparent;
+            btnNovo.Background = true;
+            btnNovo.Background_WidthPen = 4F;
+            btnNovo.BackgroundPen = true;
+            btnNovo.ColorBackground = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorBackground_1 = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorBackground_2 = Color.FromArgb(41, 63, 86);
+            btnNovo.ColorBackground_Pen = Color.FromArgb(29, 200, 238);
+            btnNovo.ColorLighting = Color.FromArgb(29, 200, 238);
+            btnNovo.ColorPen_1 = Color.FromArgb(37, 52, 68);
+            btnNovo.ColorPen_2 = Color.FromArgb(41, 63, 86);
+            btnNovo.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
+            btnNovo.Effect_1 = true;
+            btnNovo.Effect_1_ColorBackground = Color.FromArgb(29, 200, 238);
+            btnNovo.Effect_1_Transparency = 25;
+            btnNovo.Effect_2 = true;
+            btnNovo.Effect_2_ColorBackground = Color.White;
+            btnNovo.Effect_2_Transparency = 20;
+            btnNovo.Font = new Font("Arial", 11F);
+            btnNovo.ForeColor = Color.FromArgb(245, 245, 245);
+            btnNovo.Lighting = false;
+            btnNovo.LinearGradient_Background = false;
+            btnNovo.LinearGradientPen = false;
+            btnNovo.Location = new Point(35, 154);
+            btnNovo.Name = "btnNovo";
+            btnNovo.PenWidth = 15;
+            btnNovo.Rounding = true;
+            btnNovo.RoundingInt = 70;
+            btnNovo.Size = new Size(130, 50);
+            btnNovo.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            btnNovo.TabIndex = 2;
+            btnNovo.Tag = "Cyber";
+            btnNovo.TextButton = "Novo";
+            btnNovo.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            btnNovo.Timer_Effect_1 = 5;
+            btnNovo.Timer_RGB = 300;
+            btnNovo.Click += btnNovo_Click;
+            // 
             // FrmUsuarios
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnNovo);
+            Controls.Add(btnSalvar);
             Controls.Add(btnVoltar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FrmUsuarios";
@@ -88,5 +174,7 @@
         #endregion
 
         private ReaLTaiizor.Controls.CyberButton btnVoltar;
+        private ReaLTaiizor.Controls.CyberButton btnSalvar;
+        private ReaLTaiizor.Controls.CyberButton btnNovo;
     }
 }
--- Comandas/FrmUsuarios.cs
+++ Comandas/FrmUsuarios.cs
@@ -2,6 +2,8 @@
 {
     public partial class FrmUsuarios : Form
     {
+        private bool ehNovo;
+
         public FrmUsuarios()
         {
             InitializeComponent();
@@ -10,6 +12,46 @@
         private void btnVoltar_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (ehNovo)
+                CriarUsuario();
+            else
+                AtualizarUsuario();
+        }
+
+        private void AtualizarUsuario()
+        {
+            using (var banco = new BananaContext())
+            {
+                var usuario = banco
+                    .Usuarios
+                    .Where(e => e.Id == 1)
+                    .FirstOrDefault();
+                usuario.Nome = "Natasha";
+                usuario.Email = "nat@gmail.com";
+                banco.SaveChanges();
+            }
+        }
+
+        private void CriarUsuario()
+        {
+            using (var banco = new BananaContext())
+            {
+                var novoUsuario = new Usuario();
+                novoUsuario.Nome = "Rafael";
+                novoUsuario.Email = "rafaelv_s@hotmail.com";
+                novoUsuario.Senha = "123";
+                banco.Usuarios.Add(novoUsuario);
+                banco.SaveChanges();
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            ehNovo = true;
         }
     }
 }
